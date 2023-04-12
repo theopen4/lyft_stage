cc538573c5c0087983c2027ddc0f88fe65b6a565
theopen4 <kielze86@gmail.com>
2023-04-12 23:22:48 +0100

    class carfactory

---
+++ lyft_stage_projet/car_factory.py
@@ -0,0 +1,12 @@
+from battery.nubbin_batterry import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+from car import Car
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+
+class CarFactory:
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(current_date, last_service_date)
