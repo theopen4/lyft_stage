2b0094cc0427bde7ae3387e99b8def024fd32afe
theopen4 <kielze86@gmail.com>
2023-04-12 14:27:23 +0100

    init class

---
+++ lyft_stage_projet/Car.py
@@ -0,0 +1,9 @@
+from serviceable import Serviceable
+
+class Car(Serviceable):
+    def __init__(self,engine,battery):
+        self.engine = engine
+        self.battery = battery
+
+    def need_service(self):
+        return self.engine.need_service() or self.battery.need_service()
---
+++ lyft_stage_projet/serviceable.py
@@ -0,0 +1,5 @@
+from abc import ABC, abstractmethod
+class Serviceable(ABC):
+    @abstractmethod
+    def need_service(self):
+     pass
